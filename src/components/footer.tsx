832211998ca04b0c9ce7e8403e42e99af89f4a3c
Lollimor <etap-09@etap-09s-MacBook-Air.local>
2025-10-06 09:56:56 +0100

    feat: footer links added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,38 +16,56 @@ const Footer = ({ onBackToTop }: { onBackToTop: () => void }) => {
         You can view more of my works at;
       </span>
       <div className="flex items-center gap-[3.5rem] mb-[2.375rem]">
-        <div className="flex flex-col gap-2 items-center justify-center max-w-[7rem]">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://behance.net/quadrimorin"
+          className="flex flex-col gap-2 items-center justify-center max-w-[7rem]"
+        >
           <div className="w-[7rem] h-[7rem] flex items-center justify-center rounded-[0.75rem] bg-white">
             <BehanceIcon width={56} height={56} />
           </div>
           <span className="font-causten text-center font-regular text-white text-[1.5rem] leading-tight">
             Visit my Behance
           </span>
-        </div>
-        <div className="flex flex-col gap-2 items-center justify-center max-w-[7rem]">
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/quadmor/"
+          className="flex flex-col gap-2 items-center justify-center max-w-[7rem]"
+        >
           <div className="w-[7rem] h-[7rem] flex items-center justify-center rounded-[0.75rem] bg-white">
             <LinkedinIcon width={56} height={56} />
           </div>
           <span className="font-causten text-center font-regular text-white text-[1.5rem] leading-tight">
             Visit my LinkedIn
           </span>
-        </div>
-        <div className="flex flex-col gap-2 items-center justify-center max-w-[7rem]">
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://drive.google.com/file/u/0/d/1IR7KcUJ-AhLLcHaqBAmNEAXeO2hbPF4y/view?usp=sharing&pli=1"
+          className="flex flex-col gap-2 items-center justify-center max-w-[7rem]"
+        >
           <div className="w-[7rem] h-[7rem] flex items-center justify-center rounded-[0.75rem] bg-white">
             <ResumeIcon width={56} height={56} />
           </div>
           <span className="font-causten text-center font-regular text-white text-[1.5rem] leading-tight">
             Download Resume
           </span>
-        </div>
-        <div className="flex flex-col gap-2 items-center justify-center max-w-[7rem]">
+        </a>
+        <a
+          href="mailto:quadri.morin@example.com"
+          className="flex flex-col gap-2 items-center justify-center max-w-[7rem]"
+        >
           <div className="w-[7rem] h-[7rem] flex items-center justify-center rounded-[0.75rem] bg-white">
             <EmailIcon />
           </div>
           <span className="font-causten text-center font-regular text-white text-[1.5rem] leading-tight">
             Send an Email
           </span>
-        </div>
+        </a>
       </div>
       <div
         onClick={onBackToTop}
